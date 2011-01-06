71fb5419dae90b1e5cb0c17d31cf8b3cd9c686fd
katmagic <the.magical.kat@gmail.com>
2011-01-06 00:32:29 -0500

    Allow getcert.py to be executed.

--- getcert.py
+++ getcert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import hashlib
 import socket
 import ssl
@@ -25,3 +26,46 @@ def get_cert_digest(domain, port=443, digest=hashlib.sha1):
 	digest = re.sub('(..)', r'\1:', digest)[0:-1].upper()
 
 	return digest
+
+if __name__ == '__main__':
+	import optparse
+
+	op = optparse.OptionParser(
+		usage='%prog [options] host',
+		description="Get the digest of a host's SSL certificate.",
+		version="0.1",
+	)
+
+	op.add_option(
+		"-p",
+		"--port",
+		type=int,
+		default=443,
+		help="the port we should connect to (default: %default)"
+	)
+
+	digests = optparse.OptionGroup(op, "Digest Choices (default: sha1)")
+	for dgst in ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'):
+		digests.add_option(
+			"--"+dgst,
+			dest='digest',
+			action='store_const',
+			const=getattr(hashlib, dgst)
+		)
+	op.add_option_group(digests)
+	op.set_default('digest', hashlib.sha1)
+
+	try:
+		(opts, (host,)) = op.parse_args()
+	except ValueError:
+		op.print_help()
+		exit(1)
+
+	try:
+		digest = get_cert_digest(host, port=opts.port, digest=opts.digest)
+	except socket.gaierror:
+		exit("We couldn't connect to {0}:{1}".format(host, opts.port))
+	except ssl.SSLError:
+		exit("The remote host dosn't support SSL.")
+
+	print(digest)
